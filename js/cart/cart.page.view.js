d816a8c03f8149e21afd31e77b91f9656daf32e8
Molibog Roman <90984207+liner-exe@users.noreply.github.com>
2025-12-21 21:19:29 +0300

    Cart: Added currency sign at 0

--- js/cart/cart.page.view.js
+++ js/cart/cart.page.view.js
@@ -9,7 +9,7 @@ export const CartPageView = {
 
         if (cart.length === 0) {
             items.innerHTML = "<p style='color: #fff'>Корзина пуста.</p>";
-            total.textContent = (0).toString();
+            total.textContent = (0).toString() + '₽';
             totalItems.textContent = (0).toString();
             return;
         }
